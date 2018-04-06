1ae41dd714bf9bd13b43de5b11ce8a3d273dab4a
Jiaxi Gu <jiaxi.gu@epfl.ch>
2018-04-06 19:30:33 +0200

    some tests and badge in README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/Jiaxigu/pycogram.svg?branch=master)](https://travis-ci.org/Jiaxigu/pycogram)
+[![Coverage Status](https://coveralls.io/repos/github/Jiaxigu/pycogram/badge.svg?branch=master)](https://coveralls.io/github/Jiaxigu/pycogram?branch=master)
 # pycogram
 A minimalistic python compilation of graph algorithms.
--- test.py
+++ test.py
@@ -1,5 +1,30 @@
 #-*- encoding: UTF-8 -*-
 import unittest
+from pycogram import *
+
+class GraphTestCases(unittest.TestCase):
+        
+    def setUp(self):
+        self.graph = Graph([(1, 2, 3.), (1, 3, 2.), (2, 4, 6.), (3, 4, 1.)])
+        
+    def tearDown(self):
+        del(self.graph)
+        gc.collect()
+    
+    def test_len(self):
+        self.assertEqual(len(self.graph), 4)
+    
+    def test_add(self):
+        self.graph.add(4, 5)
+        self.assertEqual(len(self.graph), 5)
+
+def suite_loader():
+    test_cases = (GraphTestCases,)
+    suite = unittest.TestSuite()
+    for test_case in test_cases:
+        tests = unittest.defaultTestLoader.loadTestsFromTestCase(test_case)
+        suite.addTests(tests)
+    return suite
 
 if __name__ == '__main__':
-    pass
+    unittest.main(defaultTest='suite_loader')
